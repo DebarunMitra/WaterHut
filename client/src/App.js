80bab2570df538692856b068dcd6ae5c69647c04
DebarunMitra <debarun96mitra@gmail.com>
2019-11-23 23:38:47 +0530

    app.js modified

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,19 @@ import React, {Component} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "jquery/dist/jquery.min.js";
-import NavBar from './components/AppNavBar/NavBar'
+import NavBar from './components/AppNavBar/NavBar';
+import Login from './components/LoginInterface/Login';
+
+
+//    <NavBar />
 
 class App extends Component{
 
   render(){
     return (
       <div className="App">
-        <NavBar />
-        <h1>WaterHut</h1>
+        <Login />
+        <h6>WaterHut</h6>
       </div>
     );
   }
