0bb2cca6252503c601aad0c4b84f0b43bf7f992c
DebarunMitra <debarun96mitra@gmail.com>
2019-11-24 00:46:57 +0530

    login.js modified

--- client/src/components/LoginInterface/Login.js
+++ client/src/components/LoginInterface/Login.js
@@ -1,5 +1,6 @@
 import React ,{Component} from 'react';
 import Logo from '../../waterHut.png';
+import './Login.style.css';
 
 class Login extends Component{
 
@@ -7,9 +8,9 @@ class Login extends Component{
     return (
       <form className="p-5">
            <img className="mb-4" src={Logo} alt="" width="100" height="100" />
-           <h1 className="h1 mb-3 font-weight-normal text-primary">WaterHut</h1>
-           <h1 className="h5 mb-3 font-weight-normal text-info">Smart Water Solution</h1>
-           <button className="col-2 m-auto mb-3 btn btn-primary btn-fixed" type="submit">Sign in with Google</button>
+           <h1 className="h1 mb-3 font-weight-normal title">WaterHut</h1>
+           <h1 className="h5 mb-3 font-weight-normal quote">Smart Water Solution</h1>
+           <button className="col-2 mt-4 button-color" type="submit">Sign in with Google</button>
            <p className="mt-5 mb-3 text-muted">WaterHut &copy; 2019-2020</p>
       </form>
     );
