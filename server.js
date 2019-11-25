c610f3a7a96db3d841d0f99f0b995af54a1975ee
DebarunMitra <debarun96mitra@gmail.com>
2019-11-25 18:05:01 +0530

    db connection , express parser added

--- server.js
+++ server.js
@@ -13,3 +13,17 @@ require('./config/passport')(passport);
 
 // Load Routes
 const auth = require('./routes/auth');
+
+const app=express();
+
+//body parser middleware
+app.use(express.json());
+
+
+//db path
+const db=require('./config/keys').mongoURI;
+//const db=config.get('mongoURI');
+
+//db connection
+mongoose.connect(db, {useNewUrlParser: true,useUnifiedTopology: true,useCreateIndex:true})
+.then(()=>console.log('MongoDB connected...')).catch(err=>console.log('ERROR: '+err));
