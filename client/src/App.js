f6cb9075ac9374288b2dcc0d23e2073b1c8aa7f3
DebarunMitra <debarun96mitra@gmail.com>
2019-11-26 22:41:47 +0530

    app.js modified

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,34 @@
-import React, {
-  Component
-} from 'react';
+import React,{Component} from 'react';
+import {BrowserRouter,Route} from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "jquery/dist/jquery.min.js";
 import NavBar from './components/AppNavBar/NavBar';
 import Login from './components/LoginInterface/Login';
-
-
-//    <NavBar />
+import About from './components/About/About';
+import {connect} from 'react-redux';
+import {fetchUserAction} from './actions/authAction';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.fetch_user();
+  }
 
   render() {
-    return ( <
-      div className = "App" >
-      <
-      Login / >
-      <
-      h6 > WaterHut < /h6> <
-      /div>
-    );
-  }
+      return (
+        <BrowserRouter>
+          <Route exact path="/" component={Login} />
+          <Route exact path="/navbar" component={NavBar} />
+          <Route exact path="/about" component={About} />
+        </BrowserRouter>
+      );
+    }
 }
 
-export default App;
+const mapDispathToProps = (dispatch)=>{
+  return {
+    fetch_user:()=>{dispatch(fetchUserAction())}
+  };
+}
+
+export default connect(null,mapDispathToProps)(App);
