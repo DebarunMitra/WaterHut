9059a4c37dd4bdfeaf774767b7355f334e0e0d63
DebarunMitra <debarun96mitra@gmail.com>
2019-11-30 10:29:32 +0530

    consumer model

---
+++ models/Consumer.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+
+const ConsumerSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  },
+  person: {
+    type: Number,
+    required: true
+  },
+  tap: {
+    type: Number,
+    required: true
+  },
+  flush: {
+    type: Number,
+    required: true
+  },
+  shower: {
+    type: Number,
+    required: true
+  },
+  washingMachine: {
+    type: Number,
+    required: true
+  },
+  dishWasher: {
+    type: Number,
+    required: true
+  },
+  fridge: {
+    type: Number,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Consumer = mongoose.model('consumers', ConsumerSchema);
