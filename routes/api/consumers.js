230869d1a733744615250fc27935fa8bb9ad72a2
DebarunMitra <debarun96mitra@gmail.com>
2019-11-30 10:23:39 +0530

    consumers item routes

---
+++ routes/api/consumers.js
@@ -0,0 +1,105 @@
+const express = require('express');
+const request = require('request');
+const config = require('config');
+const router = express.Router();
+const auth = require('../../middleware/auth');
+const {
+    check,
+    validationResult
+} = require('express-validator');
+
+const Consumer = require('../../models/Consumer');
+const User = require('../../models/User');
+
+// @route  GET api/profile/me
+// @desc   Get users profile
+// @access Private
+router.get('/consumeData', auth, async (req, res) => {
+
+    try {
+        const consumer = await Consumer.findOne({
+            user: req.user.id
+        }).populate('user','person', 'tap', 'shower');
+
+        if (!consumer) {
+            return res.status(400).json({
+                msg: 'There is no consuming items'
+            });
+        }
+        res.json(consumer);
+
+    } catch (err) {
+        console.erroe(err.message);
+        res.status(500).send('Server Error');
+    }
+
+});
+
+// @route  Post api/profile
+// @desc   Create or update user profile
+// @access Private
+
+router.post('/', [auth, [
+    check('person', 'Status id required').not().isEmpty(),
+    check('tap', 'Skills are required').not().isEmpty()
+]], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({
+            errors: errors.array()
+        });
+    }
+
+    const {
+      person,
+      tap,
+      shower,
+      flush,
+      washingMachine,
+      dishWasher,
+      fridge
+    } = req.body;
+
+    //Build Profile Object
+    const consumerFields = {};
+    consumerFields.user = req.user.id;
+    if (person) consumerFields.person = person;
+    if (tap) consumerFields.tap = tap;
+    if (shower) consumerFields.shower = shower;
+    if (flush) consumerFields.flush = flush;
+    if (washingMachine) consumerFields.washingMachine = washingMachine;
+    if (dishWasher) consumerFields.dishWasher = dishWasher;
+    if (fridge) consumerFields.fridge = fridge;
+
+//console.log(profileFields.social);
+//res.send('ok');
+     try {
+         let consumer = await Consumer.findOne({
+             user: req.user.id
+          });
+
+        if (consumer) {
+            //Updating Profile
+            consumer= await Consumer.findOneAndUpdate({
+                user: req.user.id
+            }, {
+                $set: consumerFields
+            }, {
+                new: true
+            });
+            return res.json(consumer);
+        }
+
+        //Creting a Profile
+        consumer = new Consumer(consumerFields);
+      //  console.log(profile);
+        await consumer.save();
+        res.json(consumer);
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+module.exports = router;
