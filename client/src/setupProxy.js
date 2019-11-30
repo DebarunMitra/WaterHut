00a091312ed4701b73fd409ba2bbe5e12974332a
DebarunMitra <debarun96mitra@gmail.com>
2019-11-30 11:14:19 +0530

    proxu setup for development

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,6 +1,18 @@
 const proxy = require('http-proxy-middleware');
 
 module.exports = (app)=>{
-  app.use(proxy('/auth/google', { target: 'http://localhost:5020' }));
-  app.use(proxy('/auth/*', { target: 'http://localhost:5020' }));
+  //app.use(proxy('/api/users/*', { target: 'http://localhost:5020',  changeOrigin: true}));
+  app.use(
+  proxy("/api/users", {
+    target: "http://localhost:5020",
+    changeOrigin: true,
+    onProxyReq(proxyReq) {
+      if (proxyReq.getHeader("origin")) {
+        proxyReq.setHeader("origin", "http://localhost:5020")
+      }
+    },
+    pathRewrite: { "^/api/users": "" },
+    logLevel: "debug",
+  })
+)
 };
