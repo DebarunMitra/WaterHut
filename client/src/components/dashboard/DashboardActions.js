9c9b31649c2d6d8f881fe5345a3063a5170b684a
DebarunMitra <debarun96mitra@gmail.com>
2019-11-30 15:33:20 +0530

    actions modified

--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -5,15 +5,7 @@ import {Link} from 'react-router-dom';
 const DashboardActions=()=>{
   return (
     <div className="dash-buttons">
-         <Link to="/edit-consumer" className="btn btn-light"
-           ><i className="fas fa-user-circle text-primary"></i> Edit Your details</Link
-         >
-         <Link to="/add-consumer" className="btn btn-light"
-           ><i className="fab fa-black-tie text-primary"></i> Add Experience</Link
-         >
-         <Link to="/add-consumer" className="btn btn-light"
-           ><i className="fas fa-graduation-cap text-primary"></i> Add Education</Link
-         >
+         <Link to="/edit-consumer" className="btn btn-light"><i className="fas fa-user-circle text-primary"></i> Edit Your details</Link>
        </div>
   );
 }
