19b5ade68533c8483fcc632ec187e20f85557273
DebarunMitra <debarun96mitra@gmail.com>
2019-11-30 19:15:57 +0530

    link removed

--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -5,7 +5,8 @@ import {Link} from 'react-router-dom';
 const DashboardActions=()=>{
   return (
     <div className="dash-buttons">
-         <Link to="/edit-consumer" className="btn btn-light"><i className="fas fa-user-circle text-primary"></i> Edit Your details</Link>
+         <Link to="/edit-consumer" className="btn btn-light"
+           ><i className="fas fa-user-circle text-primary"></i> Edit Your details</Link>
        </div>
   );
 }
