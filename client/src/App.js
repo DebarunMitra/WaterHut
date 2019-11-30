e2059c6b374415b9ac5a037e35ce8060e28296e0
DebarunMitra <debarun96mitra@gmail.com>
2019-11-30 11:06:50 +0530

    app.js modified

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,56 @@
-import React,{Component} from 'react';
-import {BrowserRouter,Route} from 'react-router-dom';
+import React,{Fragment, useEffect} from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import "jquery/dist/jquery.min.js";
-import NavBar from './components/AppNavBar/NavBar';
-import Login from './components/LoginInterface/Login';
-import About from './components/About/About';
-import {connect} from 'react-redux';
-import {fetchUserAction} from './actions/authAction';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
+import CreateProfile from './components/profile-forms/CreateProfile';
+import EditProfile from './components/profile-forms/EditProfile';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 
-class App extends Component {
-  componentDidMount() {
-    this.props.fetch_user();
-  }
 
-  render() {
-      return (
-        <BrowserRouter>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/navbar" component={NavBar} />
-          <Route exact path="/about" component={About} />
-        </BrowserRouter>
-      );
-    }
+//redux
+import {Provider} from 'react-redux';
+import store from './store';
+import {loadUser} from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+
+
+
+if(localStorage.token){
+  setAuthToken(localStorage.token);
 }
 
-const mapDispathToProps = (dispatch)=>{
-  return {
-    fetch_user:()=>{dispatch(fetchUserAction())}
-  };
-}
 
-export default connect(null,mapDispathToProps)(App);
+////now all our component can access the state
+const App=()=>{
+  useEffect(()=>{
+    store.dispatch(loadUser());
+  },[]);
+
+  return (
+  <Provider store={store}>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <Alert />
+      <section className="container">
+          <Switch>
+            <Route exact path='/register' component={Register} />
+            <Route exact path='/login' component={Login} />
+            <PrivateRoute exact path='/dashboard' component={Dashboard} />
+            <PrivateRoute exact path='/create-profile' component={CreateProfile} />
+              <PrivateRoute exact path='/edit-profile' component={EditProfile} />
+          </Switch>
+      </section>
+      </Fragment>
+  </Router>
+  </Provider>
+)};
+
+export default App;
