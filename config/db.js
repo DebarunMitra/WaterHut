f9f9a90bbad2cdc75b085d36ce12dea1912d4672
DebarunMitra <debarun96mitra@gmail.com>
2019-12-01 18:01:58 +0530

    db module updated

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ const db = config.get("mongoURI");
 
 const connectDB = async () => {
   try {
+    console.log("Please wait...");
     await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
