adcdf52ba1dd514a51c0419ac609a9befe5f43ad
DebarunMitra <debarun96mitra@gmail.com>
2019-12-01 22:45:59 +0530

    item details modified

--- client/src/components/dashboard/ItemDetails.js
+++ client/src/components/dashboard/ItemDetails.js
@@ -1,38 +1,83 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {Fragment, useState, useEffect} from 'react';
+import {Link, withRouter} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import {getCurrentConsumer} from '../../actions/consumer';
+
+const ItemDetails=({
+  consumer:{consumer,loading},
+  getCurrentConsumer,history})=>{
+  const [formData, setFormData]=useState({
+      person:'',
+      tap:'',
+      shower:'',
+      flush:'',
+      washingMachine:'',
+      dishWasher:'',
+      filter:''
+  });
+
+  useEffect(()=>{
+    getCurrentConsumer();
+    setFormData({
+    person: loading || !consumer.person ? '' : consumer.person,
+    tap: loading || !consumer.tap ? '' : consumer.tap,
+    shower: loading || !consumer.shower ? '' : consumer.shower,
+    flush: loading || !consumer.flush ? '' : consumer.flush,
+    washingMachine: loading || !consumer.washingMachine ? '' : consumer.washingMachine,
+    dishWasher: loading || !consumer.dishWasher ? '' : consumer.dishWasher,
+    filter: loading || !consumer.filter ? '' : consumer.filter
+  });
+}, [loading, getCurrentConsumer]);
 
 
-const ItemDetails=()=>{
+  const {
+    person,
+    tap,
+    shower,
+    flush,
+    washingMachine,
+    dishWasher,
+    filter
+  }=formData;
+
   return (
     <div>
-    <h2 className="my-2">Experience Credentials</h2>
-       <table className="table">
-       <thead>
-         <tr>
-           <th>Company</th>
-           <th className="hide-sm">Title</th>
-           <th className="hide-sm">Years</th>
-         </tr>
-       </thead>
-       <tbody>
-         <tr>
-           <td>Tech Guy Web Solutions</td>
-           <td className="hide-sm">Senior Developer</td>
-           <td className="hide-sm">
-             02-03-2009 - 01-02-2014
-           </td>
-         </tr>
-         <tr>
-           <td>Traversy Media</td>
-           <td className="hide-sm">Instructor & Developer</td>
-           <td className="hide-sm">
-             02-03-2015 - Now
-           </td>
-         </tr>
-       </tbody>
-       </table>
-  </div>
+     <h2 className="my-2">Water Consuming Item's Details:</h2>
+        <table className="table">
+        <thead>
+          <tr>
+            <th className="hide-sm">TAP</th>
+            <th className="hide-sm">SHOWER</th>
+            <th className="hide-sm">FLUSH</th>
+            <th className="hide-sm">WASHING MACHINE</th>
+            <th className="hide-sm">DISH WASHER</th>
+            <th className="hide-sm">WATER FILTER</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="hide-sm">{tap}</td>
+            <td className="hide-sm">{shower}</td>
+            <td className="hide-sm">{flush}</td>
+            <td className="hide-sm">{washingMachine}</td>
+            <td className="hide-sm">{dishWasher}</td>
+            <td className="hide-sm">{filter}</td>
+          </tr>
+        </tbody>
+        </table>
+   </div>
   );
 }
 
-export default ItemDetails;
+
+ItemDetails.propTypes={
+  getCurrentConsumer: PropTypes.func.isRequired,
+  consumer: PropTypes.object.isRequired
+}
+
+const mapStateToProps=state=>({
+  consumer:state.consumer
+})
+
+export default connect(mapStateToProps,{getCurrentConsumer})(withRouter(ItemDetails));
